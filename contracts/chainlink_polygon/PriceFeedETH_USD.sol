a7f650ca222d7e9d5510d6ea0ea9f2eb79fed115
philipphoertnagl <ic22b116@technikum-wien.at>
2024-12-07 02:12:58 +0100

    adding polygon, avalanche, base smart contract

---
+++ contracts/chainlink_polygon/PriceFeedETH_USD.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// Import the Chainlink Aggregator interface
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract PriceConsumer {
+    AggregatorV3Interface internal priceFeed;
+
+    // Constructor sets the address of the price feed (ETH/USD on Sepolia)
+    constructor() {
+        priceFeed = AggregatorV3Interface(0xF0d50568e3A7e8259E16663972b11910F89BD8e7);
+    }
+
+    // Function to fetch the latest price
+    function getLatestPrice() public view returns (int) {
+        (
+            , 
+            int price, //latest price
+            , 
+            , 
+          
+        ) = priceFeed.latestRoundData();
+        return price;
+    }
+}
